0d846de215eacbe3fa8b123a3d710a201fc2ba0a
Jimin <kahin6105@gmail.com>
2021-04-09 18:18:15 +0900

    use laravel

---
+++ resources/views/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <title>해리포터 방탈출</title>
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
+    <link rel="stylesheet" type="text/css" href="./css/index.css">
+    <link rel="icon" href="./images/icon.png">
+  </head>
+  <body>
+    <div id="wrap">
+      <header>
+        <a href="index.php"><img src="./images/logo.png" alt="logo"></a>
+        <p>지금까지 이런 방탈출은 없었다!<br>당신의 눈앞에 마법의 세계가 펼쳐진다!!</p>
+      </header>
+      <section>
+        <div class="container">
+          <img src="./images/bookcover_1.jpg" alt="the Philosopher's Stone">
+          <button class="btn" onclick="location.href = 'theme.php#theme1';">더 알아보기</button>
+        </div>
+        <div class="container">
+          <img src="./images/bookcover_4.jpg" alt="the Goblet of Fire">
+          <button class="btn" onclick="location.href = 'theme.php#theme2';">더 알아보기</button>
+        </div>
+        <div class="container">
+          <img src="./images/bookcover_7.jpg" alt="the Deathly Hallows">
+          <button class="btn" onclick="location.href = 'theme.php#theme3';">더 알아보기</button>
+        </div>
+      </section>
+    </div>
+  </body>
+</html>
---
+++ resources/views/theme.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <title>해리포터 방탈출</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/theme.css">
+    <link rel="icon" href="../images/icon.png">
+  </head>
+  <body>
+    <div id="wrap">
+      <header>
+        <a href="index.php"><img src="../images/logo.png" alt="logo"></a>
+      </header>
+      <nav>
+        <div class="navElement" style="background-image: url('../images/parchment.jpg')">
+          <a href="theme.php">테 마</a>
+        </div>
+        <div class="navElement">
+          <a href="book.php">예 약</a>
+        </div>
+        <div class="navElement">
+          <a href="forum.php">후 기</a>
+        </div>
+      </nav>
+      <section class="themeSection">
+        <div class="theme" id="theme1">
+          <img src="../images/bookcover_1.jpg" alt="the Philosopher's Stone">
+          <h1>해리포터와 마법사의 돌</h1>&nbsp;&nbsp;난이도: <span class="yellowColored">★★★</span>☆☆<br>
+          &nbsp;&nbsp;어둠의 마왕 볼드모트에게 부모를 잃고 홀로 살아남은 아이, 해리는 안전을 위해 마법사가 아닌 사람인 ‘머글’ 페투니아 이모와 버논 이모부 손에 길러지며 불우한 어린 시절을 보낸다. <br>&nbsp;&nbsp;열한 번째 생일날, 해그리드를 통해 자신이 마법사라는 사실을 알게 된 해리는 호그와트 마법학교에 입학해 헤르미온느 그레인저, 론 위즐리라는 친구들과 함께 학교생활을 시작하게 된다.<br>&nbsp;&nbsp;그러던 중 비밀의 지하실에 대한 존재를 알게 되고, 그곳에서 마법사의 돌을 찾는 엄청난 모험을 하게 되는데...
+          <button class="toBooking" onclick="location.href = 'book.php';">바로 예약하기</button>
+        </div>
+        <div class="theme" id="theme2">
+          <img src="../images/bookcover_4.jpg" alt="the Goblet of Fire">
+          <h1>해리포터와 불의 잔</h1>&nbsp;&nbsp;난이도: <span class="yellowColored">★★★★</span>☆<br>
+            &nbsp;&nbsp;마법사 세계의 최대 게임인 퀴디치 월드컵 중 볼드모트의 상징인 어둠의 표식이 밤하늘에 떠오른다. 해리를 비롯한 마법사 세계가 다시 불안에 떠는 가운데 호그와트에서는 유럽의 유명한 세 마법학교에서 각 한 명씩의 대표를 선발하여 겨루는 트리위저드 대회가 개최된다.<br>&nbsp;&nbsp;17세 이하는 출전할 수 없을 정도로 위험한 이 대회에 이미 호그와트의 대표로 케드릭 디고리가 뽑혔는데도, 전례 없이 네 번째 대표로 아직 나이 어린 해리가 또 뽑혀 구설수에 오른다. 누가 불의 잔을 조작해 해리가 뽑히게 된 것인지조차 모르는 상황에서 시합은 속행되는데...
+          <button class="toBooking" onclick="location.href = 'book.php';">바로 예약하기</button>
+        </div>
+        <div class="theme" id="theme3">
+          <img src="../images/bookcover_7.jpg" alt="the Deathly Hallows">
+          <h1>해리포터와 죽음의 성물</h1>&nbsp;&nbsp;난이도: <span class="yellowColored">★★★★</span>☆<br>
+            &nbsp;&nbsp;열일곱 살이 되기 직전, 더즐리 가에 걸린 보호마법이 걷히기 전에 친구들의 미끼작전으로 은신처로 몸을 피한 해리는 론과 헤르미온느와 함께 덤블도어의 뜻을 이어 호크룩스를 찾기로 결심한다.<br>&nbsp;&nbsp;덤블도어의 유품을 조사하던 중 죽음을 피하는 강력한 마법 물품인 ‘죽음의 성물’이 실제로 존재하며 그중 하나인 딱총나무 지팡이는 볼드모트가, 나머지 하나인 투명 망토는 자신이 갖고 있다는 것을 알게 된다.<br>&nbsp;&nbsp;마지막 남은 죽음의 성물이 어디 있는지, 볼드모트와 대적할 방도는 무엇인지도 모른 채, 마지막 남은 호크룩스를 파괴하고 볼드모트와의 결전을 치르기 위해 해리와 친구들은 죽음을 먹는 자들이 장악하고 있는 호그와트로 잠입하는데...
+          <button class="toBooking" onclick="location.href = 'book.php';">바로 예약하기</button>
+        </div>
+      </section>
+    </div>
+  </body>
+</html>
